fe1cba768e6f349cdc3653553575b5274c4b871f
olegosin <oosin.zerk@gmail.com>
2021-05-26 09:47:46 -0700

    forked, migrated to androidx, got rid of ios support.

--- src/android/FCMPlugin.java
+++ src/android/FCMPlugin.java
@@ -5,7 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+//import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnFailureListener;
